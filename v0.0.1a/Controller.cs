aca4624f81c397e42946910cb66a0dc0616ba390
XaQ1997 <57348621+XaQ1997@users.noreply.github.com>
2023-04-19 19:18:02 +0200

    Add files via upload

--- v0.0.1a/Controller.cs
+++ v0.0.1a/Controller.cs
@@ -1,1 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Controller : MonoBehaviour
+{
+    private float horizontalSpeed = 10f;
+    private float verticalSpeed = 2f;
+    private float moveSpeed = 0.1f;
+    private bool gamePaused = false;
+
+    [SerializeField] private KeyCode forward = KeyCode.W;
+    [SerializeField] private KeyCode backward = KeyCode.S;
+    [SerializeField] private KeyCode left = KeyCode.A;
+    [SerializeField] private KeyCode right = KeyCode.D;
+    [SerializeField] private KeyCode up = KeyCode.E;
+    [SerializeField] private KeyCode down = KeyCode.Q;
+    [SerializeField] private KeyCode pause = KeyCode.Escape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gamePaused==false)
+        {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.Rotate(0, h, 0, Space.World);
+        transform.Rotate(-v, 0, 0, Space.Self);
+
+        if (Input.GetKey(forward))
+            transform.position += transform.forward * moveSpeed;
+        if (Input.GetKey(backward))
+            transform.position -= transform.forward * moveSpeed;
+        if (Input.GetKey(left))
+            transform.position -= transform.right * moveSpeed;
+        if (Input.GetKey(right))
+            transform.position += transform.right * moveSpeed;
+        if (Input.GetKey(up))
+            transform.position += transform.up * moveSpeed;
+        if (Input.GetKey(down))
+            transform.position -= transform.up * moveSpeed;
+        }
+
+        if (Input.GetKeyDown(pause))
+        {
+            if (gamePaused)
+            {
+                Time.timeScale = 1;
+                gamePaused = false;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+            }
+        }
+    }
+}
