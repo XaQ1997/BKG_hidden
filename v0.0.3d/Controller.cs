a605f844b7de3215233e7eb43b333696cbe27cf2
XaQ1997 <57348621+XaQ1997@users.noreply.github.com>
2023-04-25 16:31:17 +0200

    Add files via upload

--- v0.0.3d/Controller.cs
+++ v0.0.3d/Controller.cs
@@ -5,22 +5,35 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(CharacterController))]
 public class Controller : MonoBehaviour
 {
     private float horizontalSpeed =10f;
     private float verticalSpeed = 2f;
     private float moveSpeed = 0.1f;
     private bool gamePaused = false;
+    private float scroll;
     private int nrSlot = 0;
 
-    private InputManager input;
-    private CharacterController controller;
+    [SerializeField] private InputAction input;
+
+    /*[SerializeField] private KeyCode forward=KeyCode.W;
+    [SerializeField] private KeyCode backward=KeyCode.X;
+    [SerializeField] private KeyCode left=KeyCode.A;
+    [SerializeField] private KeyCode right=KeyCode.D;
+    [SerializeField] private KeyCode up = KeyCode.E;
+    [SerializeField] private KeyCode down = KeyCode.C;*/
 
     [SerializeField] private KeyCode pause = KeyCode.Escape;
+    [SerializeField] private KeyCode destroy = KeyCode.Mouse0;
+    [SerializeField] private KeyCode build = KeyCode.Mouse1;
     [SerializeField] private KeyCode kill = KeyCode.K;
 
-    [SerializeField] private KeyCode[] slotKeys = new KeyCode[] { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9 };
+    [SerializeField] private KeyCode[] slotKeys = new KeyCode[] { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.Alpha0 };
+    [SerializeField] private KeyCode[] cameras = new KeyCode[] { KeyCode.F1, KeyCode.F2, KeyCode.F3 };
+
+    [SerializeField] private GameObject firstPersonCamera;
+    [SerializeField] private GameObject secondPersonCamera;
+    [SerializeField] private GameObject thirdPersonCamera;
 
     [SerializeField] private GameObject cursor;
     [SerializeField] private GameObject[] slots = new GameObject[10];
@@ -29,11 +42,13 @@ public class Controller : MonoBehaviour
     [SerializeField] private GameObject controls;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        input = input.Instance;
-
         Resume();
+
+        firstPersonCamera.SetActive(true);
+        secondPersonCamera.SetActive(false);
+        thirdPersonCamera.SetActive(false);
     }
 
     // Update is called once per frame
@@ -42,6 +57,7 @@ public class Controller : MonoBehaviour
         var blockMap = this.gameObject.GetComponent<BlockMap>().blockMap;
         var blockController = this.gameObject.GetComponent<BlockController>();
         var gameSettings = this.gameObject.GetComponent<GameSettings>();
+        scroll = Input.mouseScrollDelta.y;
 
         if (gamePaused == false)
         {
@@ -51,13 +67,47 @@ public class Controller : MonoBehaviour
             transform.Rotate(0, h, 0, Space.World);
             transform.Rotate(-v, 0, 0, Space.Self);
 
-            Vector3 movement = input.PlayerMovement();
-            controller.Move(movement * Time.deltaTime * moveSpeed);
+            /*if (Input.GetKey(forward))
+                transform.position += new Vector3(transform.forward.x, 0, transform.forward.z) * moveSpeed;
+            if (Input.GetKey(backward))
+                transform.position -= new Vector3(transform.forward.x, 0, transform.forward.z) * moveSpeed;
+            if (Input.GetKey(left))
+                transform.position -= new Vector3(transform.right.x, 0, transform.right.z) * moveSpeed;
+            if (Input.GetKey(right))
+                transform.position += new Vector3(transform.right.x, 0, transform.right.z) * moveSpeed;
+            if (Input.GetKey(up))
+                transform.position += new Vector3(0, transform.up.y, 0) * moveSpeed;
+            if (Input.GetKey(down))
+                transform.position -= new Vector3(0, transform.up.y, 0) * moveSpeed;*/
 
-            for (int i = 0; i < 10; ++i)
-                if (Input.GetKey(slotKeys[i]))
+            if (Input.GetKey(destroy))
+                blockController.DestroyBlock();
+            if (Input.GetKey(build))
+            {
+                if(slots[nrSlot])
+                    blockController.Build(slots[nrSlot]);
+            }
+
+            if (Input.GetKey(kill))
+                gameSettings.Spawn();
+
+            if (scroll != 0)
+            {
+                if (scroll > 0)
+                    nrSlot--;
+                else
+                    nrSlot++;
+
+                if (nrSlot > 9)
+                    nrSlot = 0;
+                if (nrSlot < 0)
+                    nrSlot = 9;
+            }
+
+            for (int i = 0; i < slotKeys.Length; ++i)
+                if (Input.GetKeyDown(slotKeys[i]))
                     nrSlot = i;
-            
+
             Vector3 pos = new Vector3(88 * nrSlot - 396, 0, 0);
             highlight.transform.localPosition = pos;
         }
@@ -73,9 +123,29 @@ public class Controller : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None;
                 Time.timeScale = 0;
                 gamePaused = true;
+
                 cursor.SetActive(false);
                 panel.SetActive(true);
             }
+        }
+
+        if(Input.GetKeyDown(cameras[0]))
+        {
+            firstPersonCamera.SetActive(true);
+            secondPersonCamera.SetActive(false);
+            thirdPersonCamera.SetActive(false);
+        }
+        if(Input.GetKeyDown(cameras[1]))
+        {
+            firstPersonCamera.SetActive(false);
+            secondPersonCamera.SetActive(true);
+            thirdPersonCamera.SetActive(false);
+        }
+        if(Input.GetKeyDown(cameras[2]))
+        {
+            firstPersonCamera.SetActive(false);
+            secondPersonCamera.SetActive(false);
+            thirdPersonCamera.SetActive(true);
         }
     }
 
@@ -84,6 +154,7 @@ public class Controller : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Time.timeScale = 1;
         gamePaused = false;
+
         cursor.SetActive(true);
         controls.SetActive(false);
         panel.SetActive(false);
