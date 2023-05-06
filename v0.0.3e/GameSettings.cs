a7ca737d6f1f200d492196b9d849b1fea572efe7
XaQ1997 <57348621+XaQ1997@users.noreply.github.com>
2023-05-06 09:10:49 +0200

    Add files via upload

---
+++ v0.0.3e/GameSettings.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    [SerializeField] private Vector3 spawnPosition;
+    [SerializeField] private Quaternion spawnRotation;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(Screen.fullScreen)
+            Screen.fullScreen = !Screen.fullScreen;
+
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        transform.position = spawnPosition;
+        transform.rotation = spawnRotation;
+    }
+}
