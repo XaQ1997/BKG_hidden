80857ed35c0967b6f6db967a2ee9cc9cb6efa6e1
XaQ1997 <57348621+XaQ1997@users.noreply.github.com>
2023-05-18 10:50:33 +0200

    Add files via upload

--- v0.0.4b/Blocks/BlockController.cs
+++ v0.0.4b/Blocks/BlockController.cs
@@ -19,52 +19,31 @@ public class BlockController : MonoBehaviour
 
     public void Build(GameObject prefab)
     {
-        var chunks = this.GetComponent<MapGenerator>().Chunks();
-        var blockMap = this.gameObject.GetComponent<BlockMap>().blockMap;
+        var blocks = this.GetComponent<MapGenerator>().Blocks();
+        var mapOffset = this.GetComponent<MapGenerator>().MapOffset();
 
-        if (Physics.Raycast(InCursor.position, InCursor.forward, out RaycastHit hitInfo, length*Vector3.Magnitude(InCursor.forward)))
+        if (Physics.Raycast(InCursor.position, InCursor.forward, out RaycastHit hitInfo, length * Vector3.Magnitude(InCursor.forward)))
         {
             if (hitInfo.transform.tag == Tag)
             {
-                Vector3Int pos = new Vector3Int(Mathf.RoundToInt(hitInfo.point.x+hitInfo.normal.x/2), Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2));
-
-                GameObject block = hitInfo.transform.gameObject;
-
-                int? b = null;
-
-                for (int i = 0; i < blockMap.Count; ++i)
-                    if (blockMap[i].BlockPrefab == prefab)
-                        b = i;
-
-                Destroy(block);
-                var blocks = chunks[new Vector2(pos.x / 16, pos.z / 16)].Blocks;
-                blocks[pos.x % 16, pos.y, pos.z % 16] = b;
-
-                chunks[new Vector2(pos.x / 16, pos.z / 16)] = new Chunk(new Vector2(pos.x / 16, pos.z / 16), blocks);
+                Vector3Int pos = new Vector3Int(Mathf.RoundToInt(hitInfo.point.x + hitInfo.normal.x / 2), Mathf.RoundToInt(hitInfo.point.y + hitInfo.normal.y / 2), Mathf.RoundToInt(hitInfo.point.z + hitInfo.normal.z / 2));
+                Destroy(blocks[pos.x + mapOffset.x, pos.y + mapOffset.y, pos.z + mapOffset.z]);
+                blocks[pos.x + mapOffset.x, pos.y + mapOffset.y, pos.z + mapOffset.z] = Instantiate(prefab, pos, Quaternion.identity);
             }
             else
             {
                 Vector3Int pos = new Vector3Int(Mathf.RoundToInt(hitInfo.point.x), Mathf.RoundToInt(hitInfo.point.y), Mathf.RoundToInt(hitInfo.point.z));
-                
-                GameObject block = hitInfo.transform.gameObject;
-
-                int? b=null;
-
-                for (int i = 0; i < blockMap.Count; ++i)
-                    if (blockMap[i].BlockPrefab == prefab)
-                        b = i;
-
-                Destroy(block);
-                var blocks = chunks[new Vector2(pos.x / 16, pos.z / 16)].Blocks;
-                blocks[pos.x % 16, pos.y, pos.z % 16] = b;
-
-                chunks[new Vector2(pos.x / 16, pos.z / 16)] = new Chunk(new Vector2(pos.x / 16, pos.z / 16), blocks);
+                Destroy(blocks[pos.x + mapOffset.x, pos.y + mapOffset.y, pos.z + mapOffset.z]);
+                blocks[pos.x + mapOffset.x, pos.y + mapOffset.y, pos.z + mapOffset.z] = Instantiate(prefab, pos, Quaternion.identity);
             }
         }
     }
 
     public void DestroyBlock()
     {
+        var blocks = this.GetComponent<MapGenerator>().Blocks();
+        var mapOffset = this.GetComponent<MapGenerator>().MapOffset();
+
         if (Physics.Raycast(InCursor.position, InCursor.forward, out RaycastHit hitInfo, length * Vector3.Magnitude(InCursor.forward)))
             if (hitInfo.transform.tag == Tag)
                 Destroy(hitInfo.transform.gameObject);
